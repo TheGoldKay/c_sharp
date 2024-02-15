dbd9261cf685f812874be873f154cd821cb7aa58
TheGoldKay <jhonnatanwfc@live.com>
2024-02-15 17:35:15 -0300

    intro to the lang

---
+++ intro/1console_app/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var a = 12;
+a += 1;
+Console.WriteLine("The value and type of 'a' is: " + a + " and " + a.GetType());
+Console.WriteLine("The current time is: " + DateTime.Now);
+Console.WriteLine("The type of time is: " + DateTime.Now.GetType());
